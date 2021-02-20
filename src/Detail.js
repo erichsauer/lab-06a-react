c4677ecd5283750bb813d3ca2eec24e12e81456a
Erich Sauer <erich.sauer@gmail.com>
2021-02-19 17:19:53 -0800

    hooks up new sql api

--- src/Detail.js
+++ src/Detail.js
@@ -20,12 +20,12 @@ export default class Detail extends Component {
     });
     // sends request to pokemon api and awaits pokemon list load!
     // (superagent must be installed)
-    const data = await request.get(`https://lab-06a-node.herokuapp.com/plants/${this.state.id}`);
+    const data = await request.get(`https://arcane-falls-19139.herokuapp.com/plants/${this.state.id}`);
     
     // set state.loading to false for loading spinner display end
     this.setState({ 
         loading: false,
-        plant: data.body.results,
+        plant: data.body,
     });
     }
     
@@ -58,7 +58,7 @@ export default class Detail extends Component {
                 <img src={this.state.plant.image} className="App-logo" alt="logo" />
                 <div className='description'>{this.state.plant.description}</div>
                 <div className='fragrant'>{this.state.plant.fragrant}</div>
-                <div className='safety'>{this.state.plant.safetyNotes}</div>
+                <div className='safety'>{this.state.plant.category}</div>
                 <div className='price'>€{this.state.plant.price}</div>
                 <Link to={'/'}><h3>Back To List</h3></Link>
             </main>
